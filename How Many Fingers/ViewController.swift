e3941841c4e66905662d2b88e179ccb73902e0ed
D7703_28 <d7703_28@d7703-28ui-imac.local>
2018-03-27 15:52:28 +0900

    난수생성 및 숫자 맞추기

--- How Many Fingers/ViewController.swift
+++ How Many Fingers/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txt01: UITextField!
+    @IBOutlet weak var lbl01: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func btn01(_ sender: Any) {
+        let number = arc4random_uniform(6);
+        print(number)
+        let numberString = String(number)
+        if txt01.text == numberString {
+            lbl01.text = "빙고"
+        }
+        else{
+            lbl01.text = "실해 현재 숫자는 \(number)"
+        }
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
